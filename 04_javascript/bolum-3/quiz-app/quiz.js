3f3f18c31a64e4879ad2268e91e0237967e0cd43
“sadikturan” <sadikturan@gmail.com>
2022-06-04 10:10:52 +0300

    javascript update

--- 04_javascript/bolum-3/quiz-app/quiz.js
+++
@@ -1,9 +0,0 @@
-function Quiz(sorular) {
-    this.sorular = sorular;
-    this.soruIndex = 0;
-    this.dogruCevapSayisi = 0;
-}
-
-Quiz.prototype.soruGetir = function() {
-    return this.sorular[this.soruIndex];
-}
